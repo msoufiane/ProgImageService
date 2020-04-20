9433dd86f6a027b0757316a52a77d858a405fc3a
Soufiane MGHANEN <mgh.soufiane@gmail.com>
2020-04-20 19:48:52 +0100

    splitting project settings per environment

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProgImageService.settings')
+    ENVIRONMENT = os.getenv('DJANGO_ENVIRONMENT', 'dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProgImageService.settings.{0}'.format(ENVIRONMENT))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
